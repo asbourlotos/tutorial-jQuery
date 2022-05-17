064ccb80ceff9df7d1854fa6a1744d3262671e00
Alexander Bourlotos <alexanderbourlotos@alexanders-mbp.lan>
2022-05-16 22:27:34 -0500

    181 complete.

--- index.js
+++ index.js
@@ -4,3 +4,6 @@ $("h1").addClass("big-title margin-50");
 //manipulating text with jQuery
 $("h1").text("Bye");
 $("button").html("<em>Click Me Now!</em>")
+
+//manipulating attributes with jQuery
+$("a").attr("href", "https://www.yahoo.com/");
